e8a81e0419b4696efcab192f096eb6a80cdeb3df
marisystems <215432766+marisystems@users.noreply.github.com>
2026-07-07 21:56:22 -0300

    Finish the look of the index file for the website
    Also start to split up all the modals into modules
    that webpack will string together

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+// document.querySelector(".food-menu").showModal();
+// document.querySelector(".drinks-menu").showModal();
+// document.querySelector(".drinks-menu").showModal();
--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,11 @@ export default {
   },
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/index.html"],
+    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./src/template.html",
     }),
   ],
   module: {
